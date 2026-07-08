fb7bb2f9e0ea094069b626f718bf8964f1854c7e
L3ChatNoir <108089371+L3ChatNoir@users.noreply.github.com>
2026-07-08 15:30:32 +0200

    refactor(ruff/neovim): Upgrade rules

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -5,9 +5,6 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     desc = "Auto-format Python files after saving",
     callback = function()
         local fileName = vim.api.nvim_buf_get_name(0)
-        vim.cmd(":silent !autoflake --in-place --remove-all-unused-imports --remove-unused-variables " .. fileName)
-        vim.cmd(":silent !isort --profile black --float-to-top -q " .. fileName)
-        vim.cmd(":silent !docformatter --in-place --black " .. fileName)
         vim.cmd(":silent !ruff check --fix --silent " .. fileName)
         vim.cmd(":silent !ruff format --silent " .. fileName)
         vim.cmd(":silent !ruff clean" .. fileName)
